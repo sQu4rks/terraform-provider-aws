bf643b385d71ef80065ff6be7e46ffd1469dcae3
Marcel Neidinger <mneiding@amazon.ch>
2023-04-25 13:06:21 +0200

    Added test for tags

--- internal/service/vpclattice/service_network_service_association_data_source_test.go
+++ internal/service/vpclattice/service_network_service_association_data_source_test.go
@@ -42,6 +42,42 @@ func TestAccVPCLatticeServiceNetworkServiceAssociationDataSource_basic(t *testin
 	})
 }
 
+func TestAccVPCLatticeServiceNetworkServiceAssociationDataSource_tags(t *testing.T) {
+	ctx := acctest.Context(t)
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
+	}
+
+	var servicenetworkassocc vpclattice.GetServiceNetworkServiceAssociationOutput
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_vpclattice_service_network_service_association.test"
+
+	tag := "tag0"
+	value := "value0"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.VPCLatticeEndpointID)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.VPCLatticeEndpointID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckServiceNetworkServiceAssociationDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccServiceNetworkServiceAssociationConfig_tags1(rName, tag, value),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceNetworkServiceAssociationExists(ctx, dataSourceName, &servicenetworkassocc),
+					resource.TestCheckResourceAttr(resourceName, "tags.%", "1"),
+					resource.TestCheckResourceAttr(resourceName, "tags.tag0", "value0"),
+					acctest.MatchResourceAttrRegionalARN(dataSourceName, "arn", "vpc-lattice", regexp.MustCompile(`servicenetworkserviceassociation/+.`)),
+				),
+			},
+		},
+	})
+}
+
 func testAccServiceNetworkServiceAssociationDataSourceConfig_basic(rName string) string {
 	return fmt.Sprintf(`
 	resource "aws_vpclattice_service" "test" {
@@ -61,4 +97,29 @@ func testAccServiceNetworkServiceAssociationDataSourceConfig_basic(rName string)
 		service_network_service_association_identifier  = aws_vpclattice_service_network_service_association.test.id
 	  }
 `, rName)
+}
+
+func testAccServiceNetworkServiceAssociationConfig_tags1(rName, tagKey1, tagValue1 string) string {
+	return fmt.Sprintf(`
+resource "aws_vpclattice_service" "test" {
+  name = %[1]q
+}
+
+resource "aws_vpclattice_service_network" "test" {
+  name = %[1]q
+}
+
+resource "aws_vpclattice_service_network_service_association" "test" {
+  service_identifier         = aws_vpclattice_service.test.id
+  service_network_identifier = aws_vpclattice_service_network.test.id
+
+  tags = {
+    %[2]q = %[3]q
+  }
+}
+
+data "aws_vpclattice_service_network_service_association" "test" {
+	service_network_service_association_identifier  = aws_vpclattice_service_network_service_association.test.id
+}
+`, rName, tagKey1, tagValue1)
 }
